af10ff03ef1cc4e9aeaadb32e97df18b5b47c752
Valentino Constantinou <vc1492a@gmail.com>
2017-01-02 15:23:16 -0600

    code cleanup

---
+++ static/js/map.js
@@ -0,0 +1,47 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoicmVib290IiwiYSI6ImNpeGdiaXl0MDAwMXkyeW52NTJjMm1jMHIifQ.IwhmgHr1XEF2iNDo5_eXDA';
+var geojson = [
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-118.3118677, 34.0575926]
+        },
+        properties: {
+            title: 'Los Angeles, CA',
+            description: 'Valentino Constantinou<br> Rustam Mirzaev<br> Taylor Stevens',
+            'marker-color': '#1AADC1',
+            'marker-size': 'large'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-75.2581184, 40.0049314]
+        },
+        properties: {
+            title: 'Philadelphia, PA',
+            description: 'Michael Thompson',
+            'marker-color': '#1AADC1',
+            'marker-size': 'large'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-84.0651685, 35.9586599]
+        },
+        properties: {
+            title: 'Knoxville, TN',
+            description: 'Sammy Thomas',
+            'marker-color': '#1AADC1',
+            'marker-size': 'large'
+        }
+    }
+];
+var map = L.mapbox.map('map')
+    .setView([37.8, -96], 5);
+L.mapbox.featureLayer().setGeoJSON(geojson).addTo(map);
+map.scrollWheelZoom.disable();
+var styleLayer = L.mapbox.styleLayer('mapbox://styles/mapbox/dark-v9').addTo(map);
