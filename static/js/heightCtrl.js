ce1ba6acc9a09fc525e31cbb7d597bcc0d5c7388
evolvedus <evolvedus@gmail.com>
2017-01-28 13:36:56 -0500

    cool card-height solution

---
+++ static/js/heightCtrl.js
@@ -0,0 +1,25 @@
+ function setEqualHeight(selector, triggerOnInput) {
+    if (screen.width >= 992) {
+        var elements = $(selector)
+        elements.css("height", "auto")
+        var max = Number.NEGATIVE_INFINITY;
+        $.each(elements, function(index, item) {
+            if ($(item).height() > max) {
+                max = $(item).height()
+            }
+        })
+        $(selector).css("height", max + "px")
+        if (!!triggerOnInput) {
+            $(document).on("input", selector, function() {
+                setEqualHeight(selector, false)
+            })
+        }
+        $(window).resize(function() {
+            setEqualHeight(selector, false)
+        })
+    }
+}
+setEqualHeight(".card-team", true) 
+setEqualHeight(".card-text", true) 
+setEqualHeight(".bottom-card-container", true)
+setEqualHeight(".img-fluid", true)
