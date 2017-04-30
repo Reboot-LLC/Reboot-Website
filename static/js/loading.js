a418f007f358b687122b1b18b12d0a1cea3108e4
Valentino Constantinou <vc1492a@gmail.com>
2017-04-29 21:11:26 -0700

    loading improvement, injected footer

--- static/js/loading.js
+++ static/js/loading.js
@@ -1,4 +1,44 @@
-/* to turn animation always on, disable the if statement below and re-enable introduction(); */
+/* functions we later use to disable scrolling while the loading animation is present */
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+    if (window.addEventListener) // older FF
+        window.addEventListener('DOMMouseScroll', preventDefault, false);
+    window.onwheel = preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+    window.ontouchmove  = preventDefault; // mobile
+    document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+// start by disabling scroll
+disableScroll();
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
+
+// run the loading animation
 introduction();
 
 function introduction() {
@@ -228,6 +268,7 @@ function introduction() {
         V(loadContainer, {opacity: 0}, {duration: 2000});
         V(homeContainer, {opacity: 1}, {duration: 2000});
         setTimeout(function() {
+            enableScroll();
             loadContainer.style.zIndex = "-1";
         }, 1900);
 
